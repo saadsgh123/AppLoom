ab389294fada2edb4c4ca5525907576017fb86ea
saadsgh123 <sdsghouri@gmail.com>
2024-12-14 15:07:21 +0100

    test db update

--- routes.py
+++ routes.py
@@ -18,7 +18,7 @@ def add(user_id):
     user = None
     if user_id:
         user = storage.find_one({'id': user_id})
-        return redirect("/add", user)
+        return redirect("/add/<user>", user)
     return render_template('add.html', user=user)
 
 
