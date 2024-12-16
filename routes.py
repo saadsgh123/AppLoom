ac52744165575f70a3abb1e5045934de43a62357
saadsgh123 <sdsghouri@gmail.com>
2024-12-17 00:25:09 +0100

    design jobapp.html post jobapp 2

--- routes.py
+++ routes.py
@@ -69,15 +69,5 @@ def delete(job_id):
     return redirect(url_for('index'))
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    print("Request received at /submit")
-    print("Request Data:", request.json)
-    return jsonify({"status": "success"})
-
-
-
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
