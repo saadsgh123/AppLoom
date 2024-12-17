de8c9ecc6a0c08c93d6918d6917576cffee78245
saadsgh123 <sdsghouri@gmail.com>
2024-12-17 14:42:56 +0100

    jobapp delete logic test 2

--- models/base_model.py
+++ models/base_model.py
@@ -65,4 +65,4 @@ class BaseModel:
 
     def delete(self):
         """delete the current instance from the storage"""
-        models.storage.delete_one({'id': self.id})
+        models.storage.delete_one(self, {'id': self.id})
